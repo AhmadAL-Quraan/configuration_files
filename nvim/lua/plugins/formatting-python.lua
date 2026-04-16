30da717c5de7679687ae15fa832ee03cafd32e85
Ahmad AL-Quraan <ahmadquraan.git@gmail.com>
2026-04-16 08:21:14 +0300

    Adding conform plugin

--- nvim/lua/plugins/formatting-python.lua
+++ nvim/lua/plugins/formatting-python.lua
@@ -1,16 +1,13 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.black.with({
-            extra_args = { "--line-length", "79" },
-          }),
-        },
-      })
-    end,
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      python = { "black" },
+    },
+    formatters = {
+      black = {
+        prepend_args = { "--line-length", "79" },
+      },
+    },
   },
 }
